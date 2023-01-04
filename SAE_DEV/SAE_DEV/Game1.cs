41e7473f1bd4cb96ef6ff5c4e633b6d9ad15732f
GabinLeveugle <Gabin.Leveugle@etu.univ-savoie.fr>
2023-01-04 17:28:57 +0100

    frz

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -1,50 +1,92 @@
-﻿using Microsoft.Xna.Framework;
+﻿
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
 
 namespace SAE_DEV
 {
     public class Game1 : Game
     {
+        private readonly ScreenManager _screenManager;
+        public SpriteBatch SpriteBatch { get; set; }
+        Acceuil _acceuil;
+        Jeu _jeu;
+        Fin _fin;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+
+
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+
+            _screenManager = new ScreenManager();
+            Components.Add(_screenManager);
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _acceuil = new Acceuil(this); // en leur donnant une référence au Game
+            _jeu = new Jeu(this);
+            _fin = new Fin(this);
+            _spriteBatch.Draw(ButtonTexture, ButtonRectangle, Color.White);
+
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            KeyboardState keyboardState = Keyboard.GetState();
+            ButtonRectangle = new Rectangle(ScreenX * 0.4, ScreenY * 0.5, ScreenX * 0.1, ScreenY * 0.1);
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                _screenManager.LoadScreen(_acceuil, new FadeTransition(GraphicsDevice,
+                Color.Black));
+            }
+            MouseState mouse = Mouse.GetState();
+            if (ButtonRectangle.Contains(mouse.Position) && mouse.LeftButton == ButtonState.Pressed)
+            {
+                //do stuff
 
-            // TODO: Add your update logic here
+                else if (keyboardState.IsKeyDown(Keys.Down))
+                {
+                    _screenManager.LoadScreen(_jeu, new FadeTransition(GraphicsDevice, Color.Black));
+                }
+                else if (keyboardState.IsKeyDown(Keys.Right))
+                {
+                    _screenManager.LoadScreen(_fin, new FadeTransition(GraphicsDevice,
+                    Color.Black));
+                }
+            }
+
 
             base.Update(gameTime);
         }
+
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
 
             base.Draw(gameTime);
         }
